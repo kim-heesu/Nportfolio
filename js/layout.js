aceea55ee03bd594c065c86a4da06d5f02e3f412
kim-heesu <46507288+kim-heesu@users.noreply.github.com>
2022-07-27 12:37:37 +0900

    포트폴리오 업데이트

---
+++ js/layout.js
@@ -0,0 +1,64 @@
+
+$(document).ready(function(){
+    /*aos*/
+    $(function(){
+        AOS.init();
+    });
+
+    /*slick slide*/
+    $(".page-slide").slick({
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        variableWidth: true,
+        dots: false,
+        arrows: false,
+        infinite: true,
+        autoplay: false,
+        autoplaySpeed: 3000,
+    });
+
+    $(".landing-slide").slick({
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        variableWidth: true,
+        dots: false,
+        arrows: false,
+        infinite: true,
+        autoplay: false,
+        autoplaySpeed: 3000,
+    });
+
+    // menu
+    $('.btn-menu').on('click',function(){
+        $(this).parents('header').toggleClass('menu-open');
+    });
+
+    // skill list
+    const listInfo = ['HTML5','CSS3','jQuery','javascript','Github','Sass','Photoshop','figma','Jira','SourceTree','Trello','Visual Studio Code','phpstorm']
+    let listLength = 0;
+    let appendList = setInterval(function(){
+        $('.about-skill ul').append('<li><span>'+listInfo[listLength]+'</span></li>')
+        listLength++;
+        if(listLength > listInfo.length) {
+            listLength = 0;
+            $('.about-skill ul').empty();
+        }
+    }, 1000);
+});
+
+$(window).scroll(function(){
+    var scrollValue = $(document).scrollTop();
+    var meTop = $('.about-me').offset().top;
+    var headerHeight = $('header').outerHeight();
+
+    let f = true;
+
+    if(scrollValue > meTop - headerHeight){
+        $('header').addClass('active');
+    } else {
+        $('header').removeClass('active');
+    }
+
+});
+
+
